fbe33ca0bf67a38c81e7ce9ae19f4a9c958aa418
GUSTAVO SOARES FRANCO <gustavo_franco2@estudante.sesisenai.org.br>
2025-12-02 17:00:59 -0300

    nwe css landing

--- Frontend/re-loop-app/components/reloop/Landing.jsx
+++ Frontend/re-loop-app/components/reloop/Landing.jsx
@@ -1,26 +1,49 @@
-"use client"
+"use client";
 
-import { useGlobal } from "@/context/global-context"
-import { Logo } from "./Logo"
+import { useGlobal } from "../../context/global-context";
+import { Logo } from "./Logo";
 
 export function Landing() {
-  const { navigate } = useGlobal()
+  const { navigate } = useGlobal();
 
   return (
     <div className="landing-page">
       <div className="landing-content">
-        <div className="landing-logo">
-          <Logo size="large" />
+        <div className="texto-logo">
+          <div className="landing-logo">
+            <img
+              src="./public/ReLoop_branca.svg"
+              alt="ReLoop_branca.svg"
+              className="landing-logo"
+            />
+          </div>
+          <div className="texto-landing">
+            <h1 className="landing-tagline">
+              Dê Nova Vida ao Que Você Não Usa -{" "}
+              <strong style={{}}>Entre no Reloop</strong>
+            </h1>
+            <p className="landing-description">
+              Junte-se à nossa comunidade de consumidores conscientes. Dê uma
+              segunda vida aos seus itens ou encontre tesouros de outras pessoas.
+            </p>
+          </div>
         </div>
-        <h1 className="landing-tagline">Troca & Doação Sustentável.</h1>
-        <p className="landing-description">
-          Junte-se à nossa comunidade de consumidores conscientes. Dê uma segunda vida aos seus itens ou encontre
-          tesouros de outras pessoas.
-        </p>
-        <button className="btn btn-primary btn-large" onClick={() => navigate("auth")}>
-          Começar
-        </button>
+        <div className="botoes-lading">
+          <button
+            className="btn btn-primary btn-large"
+            onClick={() => navigate("auth")}
+          >
+            Começar
+          </button>
+        </div>
+      </div>
+      <div className="img-landing">
+        <img
+          src="./public/imagem_landingPage.png"
+          alt="landing_image.svg"
+          className="landing-image"
+        />
       </div>
     </div>
-  )
+  );
 }
